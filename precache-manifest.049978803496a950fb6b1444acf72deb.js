d8f82b3e870b4be91ca59678b78d58afb0a0dd7f
Michal <michal.jablonski097@gmail.com>
2020-09-10 17:50:35 +0200

    Updates

---
+++ precache-manifest.049978803496a950fb6b1444acf72deb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b40997fb8d598832c8f71932598a20",
+    "url": "/waveex/poligon-reactJS/index.html"
+  },
+  {
+    "revision": "ae7d0d019ad0081677d6",
+    "url": "/waveex/poligon-reactJS/static/css/main.f3b71e68.chunk.css"
+  },
+  {
+    "revision": "7036880e201ca801cfeb",
+    "url": "/waveex/poligon-reactJS/static/js/2.0d6b7f6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/waveex/poligon-reactJS/static/js/2.0d6b7f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae7d0d019ad0081677d6",
+    "url": "/waveex/poligon-reactJS/static/js/main.c1b81adb.chunk.js"
+  },
+  {
+    "revision": "17f05e93afc045c554d6",
+    "url": "/waveex/poligon-reactJS/static/js/runtime-main.aadd5b0e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/waveex/poligon-reactJS/static/media/logo.5d5d9eef.svg"
+  }
+]);
